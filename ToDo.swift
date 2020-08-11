c7bd0f3bf83430b87b999ed3454e74189b0caedc
carlybrail <69254570+carlybrail@users.noreply.github.com>
2020-08-10 20:31:18 -0400

    Add files via upload

---
+++ ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  ToDoListProject
+//
+//  Created by Carly Brail on 8/10/20.
+//  Copyright © 2020 Carly Brail. All rights reserved.
+//
+import UIKit
+
+class ToDoClass {
+    var description = ""
+    var important = false
+}
